7332b84e4a6d31f033348e0891ec6a717d4ea2ac
Matt Thomason <matt@warpedcore.net>
2024-10-12 11:42:57 +0000

    Add comments to modules that don't do anything

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,8 @@
 const { Events } = require('discord.js');
 const { PresenceUpdateStatus } = require('discord.js');
 
+// Note - this is for future use and currently doesn't do anything
+
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
